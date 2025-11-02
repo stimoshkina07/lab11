ab2c41cadf0d0e8378d0b154b8fd61bc7860d856
stimoshkina07 <s.timoshkina07@gmail.com>
2025-11-02 21:36:19 +0400

    Add notes.py with initial thoughts about Git

---
+++ notes.py
@@ -0,0 +1,12 @@
+# notes.py
+# Initial thoughts about Git
+
+thoughts = [
+    "Git helps manage versions of your work.",
+    "Each change is saved as a commit.",
+    "It makes teamwork easier."
+]
+
+for line in thoughts:
+    print(line)
+
